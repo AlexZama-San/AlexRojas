c5579b1314f435092a147768104704c177f8b87c
AlexZama-San <61293885+AlexZama-San@users.noreply.github.com>
2020-02-28 08:52:44 -0600

    ultimos cambios

--- Contactos.java
+++ Contactos.java
@@ -12,4 +12,22 @@ public class Contactos {
         add=" Nombre: "+persona.getNombre()+"\n Fecha de nacimiento: "+persona.getFechaNac()+"\n Numero telefonico: "+persona.getNumTel()+"\n Correo Electronico: "+persona.getCorreoE();
         Contac.add(add);
     }
+    public static void removeContact(){
+        int elecc;
+        System.out.println("¿que contacto desea remover?");
+        int c=0;
+        while(c<Contac.size()){
+            System.out.println(c+".- "+Contac.get(c)+"\n");
+            c++;
+        }
+        elecc=lector.nextInt();
+        Contac.remove(elecc);
+    }
+    public static void MostrarLista(){
+        int c=0;
+        while(c<Contac.size()){
+            System.out.println(c+".- "+Contac.get(c)+"\n");
+            c++;
+        }
+    }
 }
--- Primera.java
+++ Primera.java
@@ -3,5 +3,7 @@ import java.util.*;
 public class Primera {
     public static void main (String []args){
         Contactos.AddContact();
+        Contactos.AddContact();
+        Contactos.MostrarLista();
     }
 }
